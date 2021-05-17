57af304d5b6d2a8cf52d95c4716bf3406a967d02
Timea Gyarmathy <gyarmathy.timi@hotmail.com>
2021-05-17 21:34:27 +0200

    Refactored GUI code and commented it nicely

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Image-Processing
 Project for the course 'Image Processing at VUB'. The topic is 'Image Retargeting &amp; Object Removal via Seam Carving'.
+
+
+```
+conda install -c anaconda numpy
+conda install -c conda-forge opencv
+conda install numba
+conda install cudatoolkit
+conda install scikit-image
+conda install -c conda-forge pysimplegui
+conda install -c conda-forge imutils
+```
--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import os
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from tkinter import filedialog
 
 import cv2
@@ -10,46 +10,93 @@ from PIL import ImageTk, Image, ImageDraw
 from SeamImage import *
 
 DEFAULT_IMAGE = 'Figures\\Castle.jpg'
+DEFAULT_SIZE = 400
 
 
 class SeamCarvingGUI(tk.Frame):
+    """
+    Class for the Seam Carving GUI layout implemented with Tkinter, the Python binding to the Tk GUI toolkit
+    """
+
     def __init__(self, master):
+        """
+        Constructor initializes class attributes and GUI layout
+        :param master: parent widget and it is an optional parameter
+        """
         super(SeamCarvingGUI, self).__init__(master)
         self.pack()
         self.master = master
 
+        """ Class attributes """
+        # For drawing
+        self.last_x, self.last_y = None, None
+        self.color = 'red'
+        self.color_code = (255, 0, 0)
+
+        # For displaying images
+        self.seam_image = None
+        # The ghost_image is not visible and it is used to retrieve the mask for eventual object removal functionality
+        self.ghost_image = Image.new("RGB", (DEFAULT_SIZE, DEFAULT_SIZE), (0, 0, 0))
+        self.draw = ImageDraw.Draw(self.ghost_image)
+
+        """ GUI bound variables """
+        self.file_path = tk.StringVar()
+        self.width_var = tk.IntVar()
+        self.height_var = tk.IntVar()
+        self.keep_shape_var = tk.IntVar()
+
+        # Initialize GUI elements
+        self.initialize_gui()
+
+    def initialize_gui(self):
+        """
+        Method called from the constructor, segmented for better readability.
+        Contains the initialization of the whole GUI window layout.
+        The layout is split into two main parts:
+
+         |--------------|
+         |  TOP FRAME   |
+         |--------------|
+         | BOTTOM FRAME |
+         |--------------|
+
+         The 'top frame' contains multiple functional components for interaction.
+         The 'bottom frame' contains the image display.
+        """
         top_frame = tk.Frame(self)
         top_frame.pack(side=tk.TOP, expand=True)
 
+        # First section in the top frame contains components for selecting the image file
         file_frame = tk.Frame(top_frame)
         file_frame.pack(side=tk.TOP, expand=True, fill=tk.X)
 
         browse_button = tk.Button(file_frame, text="Browse...", anchor='w', command=self.add_image)
         browse_button.pack(side=tk.LEFT)
 
-        self.file_path = tk.StringVar()
         label = tk.Label(file_frame, anchor='w', textvariable=self.file_path)
         label.pack(side=tk.LEFT)
 
+        # Separator lines are used between sections for better visibility
         separator1 = ttk.Separator(top_frame, orient='horizontal')
         separator1.pack(fill=tk.X)
 
+        # Second section in the top frame contains elements related to resizing the image
         resize_frame = tk.Frame(top_frame)
         resize_frame.pack(side=tk.TOP, expand=True, fill=tk.X)
 
-        label = tk.Label(resize_frame, text='Resize', width=8, anchor='w', font='Helvetica 18 bold')
+        label = tk.Label(resize_frame, text='Resize image', width=15, anchor='w', font='Helvetica 14 bold')
         label.pack(side=tk.LEFT)
 
-        self.width_var = tk.IntVar()
         width_label = tk.Label(resize_frame, text="Width:")
-        width_entry = tk.Entry(resize_frame, textvariable=self.width_var)
         width_label.pack(side=tk.LEFT)
+
+        width_entry = tk.Entry(resize_frame, textvariable=self.width_var)
         width_entry.pack(side=tk.LEFT)
 
-        self.height_var = tk.IntVar()
         height_label = tk.Label(resize_frame, text="Height:")
-        height_entry = tk.Entry(resize_frame, textvariable=self.height_var)
         height_label.pack(side=tk.LEFT)
+
+        height_entry = tk.Entry(resize_frame, textvariable=self.height_var)
         height_entry.pack(side=tk.LEFT)
 
         execute_resize_button = tk.Button(resize_frame, text="Execute", command=self.resize_image)
@@ -58,121 +105,206 @@ class SeamCarvingGUI(tk.Frame):
         reset_resize_button = tk.Button(resize_frame, text="Reset", command=self.reset_img)
         reset_resize_button.pack(side=tk.LEFT)
 
+        # Separator lines are used between sections for better visibility
         separator2 = ttk.Separator(top_frame, orient='horizontal')
         separator2.pack(fill=tk.X)
 
-        retarget_frame = tk.Frame(top_frame)
-        retarget_frame.pack(side=tk.TOP, expand=True, fill=tk.X)
+        # Third section in the top frame contains elements related to object removal
+        obj_remove_frame = tk.Frame(top_frame)
+        obj_remove_frame.pack(side=tk.TOP, expand=True, fill=tk.X)
 
-        label = tk.Label(retarget_frame, text='Retarget', width=8, anchor='w', font='Helvetica 18 bold')
+        label = tk.Label(obj_remove_frame, text='Remove object', width=15, anchor='w', font='Helvetica 14 bold')
         label.pack(side=tk.LEFT)
 
-        red_button = tk.Button(retarget_frame, text="RED", fg="red", command=self.select_red)
+        red_button = tk.Button(obj_remove_frame, text="RED", fg="red", command=self.select_red)
         red_button.pack(side=tk.LEFT)
 
-        red_button = tk.Button(retarget_frame, text="GREEN", fg="green", command=self.select_green)
-        red_button.pack(side=tk.LEFT)
+        green_button = tk.Button(obj_remove_frame, text="GREEN", fg="green", command=self.select_green)
+        green_button.pack(side=tk.LEFT)
 
-        execute_retarget_button = tk.Button(retarget_frame, text="Execute", command=self.retarget_image)
-        execute_retarget_button.pack(side=tk.LEFT)
+        keep_shape_checkbox = tk.Checkbutton(obj_remove_frame, text="Keep shape?", variable=self.keep_shape_var)
+        keep_shape_checkbox.pack(side=tk.LEFT)
 
-        reset_retarget_button = tk.Button(retarget_frame, text="Reset", command=self.reset_img)
-        reset_retarget_button.pack(side=tk.LEFT)
+        execute_obj_remove_button = tk.Button(obj_remove_frame, text="Execute", command=self.remove_object_image)
+        execute_obj_remove_button.pack(side=tk.LEFT)
 
+        reset_remove_obj_button = tk.Button(obj_remove_frame, text="Reset", command=self.reset_img)
+        reset_remove_obj_button.pack(side=tk.LEFT)
+
+        # Separator lines are used between sections for better visibility
         separator3 = ttk.Separator(top_frame, orient='horizontal')
         separator3.pack(fill=tk.X)
 
+        # Fourth section in the top frame contains elements related to content amplification
+        content_ampl_frame = tk.Frame(top_frame)
+        content_ampl_frame.pack(side=tk.TOP, expand=True, fill=tk.X)
+
+        label = tk.Label(content_ampl_frame, text='Amplify content', width=15, anchor='w', font='Helvetica 14 bold')
+        label.pack(side=tk.LEFT)
+
+        execute_amplify_button = tk.Button(content_ampl_frame, text="Execute", command=self.amplify_content)
+        execute_amplify_button.pack(side=tk.LEFT)
+
+        reset_amplify_button = tk.Button(content_ampl_frame, text="Reset", command=self.reset_img)
+        reset_amplify_button.pack(side=tk.LEFT)
+
+        # Bottom frame contains a canvas for displaying the image and drawing
         canvas_frame = tk.Frame(top_frame)
         canvas_frame.pack(side=tk.TOP, expand=True)
 
-        self.canvas = tk.Canvas(canvas_frame, width=400, height=400, bg='black')
+        self.canvas = tk.Canvas(canvas_frame, width=DEFAULT_SIZE, height=DEFAULT_SIZE, bg='black')
         self.canvas.pack(fill=tk.BOTH)
 
         self.image = ImageTk.PhotoImage(Image.fromarray(np.asarray([0, 0, 0])))
         self.image_on_canvas = self.canvas.create_image(0, 0, image=self.image, anchor='nw')
 
+        # Bind drawing functionalities to mouse left click and mouse left button hold
         self.canvas.bind("<Button-1>", self.get_x_and_y)
-        self.canvas.bind("<B1-Motion>", self.draw_smth)
-
-        self.lasx, self.lasy = None, None
-        self.color = 'red'
-        self.color_code = (255, 0, 0)
-
-        self.ghost_image = Image.new("RGB", (400, 400), (0, 0, 0))
-        self.draw = ImageDraw.Draw(self.ghost_image)
-        self.seam_image = None
+        self.canvas.bind("<B1-Motion>", self.draw_on_image)
 
     def add_image(self):
+        """
+        This method solves selecting an image file and displaying it in the GUI
+        """
+        # If there was previous drawing on the canvas, remove it
         self.canvas.delete("line")
 
+        # Prompts the user for file selection via a file dialog
         img_path = filedialog.askopenfilename(initialdir=os.getcwd(), title="Open Image",
                                               filetypes=[("Image Files", "*.png *.jpg *.jpeg")])
+        self.file_path.set(img_path)
         self.seam_image = SeamImage(img_path)
-        height, width, _ = self.seam_image.get_image().shape
-        self.width_var.set(width)
-        self.height_var.set(height)
-        self.ghost_image = self.ghost_image.resize((width, height))
-        self.draw = ImageDraw.Draw(self.ghost_image)
-        self.image = ImageTk.PhotoImage(Image.fromarray(self.seam_image.get_image()))
-        self.canvas.config(width=width, height=height)
-        self.canvas.itemconfig(self.image_on_canvas, image=self.image, anchor='nw')
+
+        # Adjust GUI to selected image
+        self.post_process()
 
     def reset_img(self):
+        """
+        Reset functionality discards any modification done to the original image
+        """
+        # Delete any drawing from the canvas
         self.canvas.delete("line")
+
         if self.seam_image is not None:
             self.seam_image.reset()
-            self.image = ImageTk.PhotoImage(Image.fromarray(self.seam_image.get_image()))
-            self.canvas.itemconfig(self.image_on_canvas, image=self.image, anchor='nw')
-            height, width, _ = self.seam_image.get_image().shape
-            self.width_var.set(width)
-            self.height_var.set(height)
-            self.ghost_image = self.ghost_image.resize((width, height))
-            self.draw = ImageDraw.Draw(self.ghost_image)
+            # Update display
+            self.post_process()
+
+    def post_process(self):
+        """
+        This method adjusts the GUI elements to any resizing change that was done to the displayed image
+        """
+        # Get the height and weight and we don't care about the number of channels
+        height, width, _ = self.seam_image.get_image().shape
+
+        # Set the width and height variables to display the updated values
+        self.width_var.set(width)
+        self.height_var.set(height)
+
+        # Adjust the canvas and the newly displayed image element accordingly
+        self.canvas.config(width=width, height=height)
+        new_image = self.seam_image.get_image().astype(np.uint8)  # Convert to 8 bit unsigned integer for display
+        self.image = ImageTk.PhotoImage(Image.fromarray(new_image))
+        self.canvas.itemconfig(self.image_on_canvas, image=self.image, anchor='nw')
+
+        # The ghost_image is used to retrieve the mask, it has to be the same size as the image
+        self.ghost_image = self.ghost_image.resize((width, height))
+        self.draw = ImageDraw.Draw(self.ghost_image)
 
     def select_red(self):
+        """
+        Set drawing color to red
+        """
         self.color = 'red'
         self.color_code = (255, 0, 0)
 
     def select_green(self):
+        """
+        Set drawing color to green
+        """
         self.color = 'green'
         self.color_code = (0, 255, 0)
 
     def get_x_and_y(self, event):
-        self.lasx, self.lasy = event.x, event.y
+        """
+        Update the last registered coordinates of a mouse event
+        :param event: I/O event, in out case a mouse click
+        """
+        self.last_x, self.last_y = event.x, event.y
 
-    def draw_smth(self, event):
-        self.canvas.create_line((self.lasx, self.lasy, event.x, event.y), fill=self.color, width=10, tag="line")
-        self.draw.line([self.lasx, self.lasy, event.x, event.y], width=10, fill=self.color_code)
-        self.lasx, self.lasy = event.x, event.y
+    def draw_on_image(self, event):
+        """
+        Draw onto the image canvas
+        :param event: I/O event, in out case a mouse drag
+        """
+        # Our drawing will be simulated by repeatedly drawing a line
+        # between the previous mouse location and the current one
+        self.canvas.create_line((self.last_x, self.last_y, event.x, event.y), fill=self.color, width=10, tag="line")
+        # Draw on the invisible mask image as well
+        self.draw.line([self.last_x, self.last_y, event.x, event.y], width=10, fill=self.color_code)
+        # Update coordinates of the last event
+        self.last_x, self.last_y = event.x, event.y
 
     def resize_image(self):
-        target_width = self.width_var.get()
-        target_height = self.height_var.get()
-        self.seam_image.resize(height=target_height, width=target_width)
-        self.image = ImageTk.PhotoImage(Image.fromarray(self.seam_image.get_image()))
-        self.canvas.config(width=target_width, height=target_height)
-        self.canvas.itemconfig(self.image_on_canvas, image=self.image, anchor='nw')
-        self.ghost_image = self.ghost_image.resize((target_width, target_height))
-        self.draw = ImageDraw.Draw(self.ghost_image)
+        """
+        Method for executing the resize via seam carving functionality
+        """
+        # Check for proper numeric input and warn the user otherwise
+        try:
+            # Retrieve input width and height
+            target_width = self.width_var.get()
+            target_height = self.height_var.get()
 
-    def retarget_image(self):
-        mask = self.ghost_image
-        r, g, b = cv2.split(np.array(mask))
+            # Check for positive input and warn the user otherwise
+            if target_height <= 0:
+                messagebox.showerror("Error!", "Please input a positive height value!")
+                return
+
+            if target_width <= 0:
+                messagebox.showerror("Error!", "Please input a positive width value!")
+                return
+
+            # Call the seam carving function
+            self.seam_image.resize(height=target_height, width=target_width)
+            # Update display
+            self.post_process()
+        except tk.TclError:
+            messagebox.showerror("Error!", "Width and height must be integers!")
+
+    def remove_object_image(self):
+        """
+        Use seam carving to remove objects from the image
+        """
+        # The ghost image contains the drawn red and green areas and black in the rest
+        color_mask = self.ghost_image
+
+        # Retrieve the red areas for removal and the green areas for keeping
+        r, g, b = cv2.split(np.array(color_mask))
         red_mask = np.where(r, 1, 0)
         green_mask = np.where(g, 1, 0)
-        # get value of checkbox
-        self.seam_image.remove_mask(mask=red_mask)
-        self.image = ImageTk.PhotoImage(Image.fromarray(self.seam_image.get_image().astype(np.uint8)))
-        height, width = self.seam_image.get_image().shape[0], self.seam_image.get_image().shape[1]
-        self.canvas.config(height=height, width=width)
-        self.canvas.itemconfig(self.image_on_canvas, image=self.image, anchor='nw')
-        self.ghost_image = self.ghost_image.resize((width, height))
-        self.draw = ImageDraw.Draw(self.ghost_image)
+
+        keep_shape = True if self.keep_shape_var.get() else False
+        self.seam_image.remove_mask(mask=red_mask, keep_shape=keep_shape)
+
+        # Remove all drawings
         self.canvas.delete("line")
+        # Update display
+        self.post_process()
+
+    def amplify_content(self):
+        """
+        Amplify content via seam carving
+        """
+        self.seam_image.amplify_content()
+        # Update display
+        self.post_process()
 
 
 if __name__ == '__main__':
+    # Initialize main window
     root = tk.Tk()
     root.title('Seam Carving')
     SeamCarvingGUI(master=root)
+
+    # This method starts up the tkinter GUI
     root.mainloop()
