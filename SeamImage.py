707877f1a6021a0d9e05e325119e092d21b2421e
Seppe Lampe <lampe.seppe@hotmail.com>
2021-05-17 15:36:22 +0200

    Updated SeamImage.py with 'resize' method from image_resizing.py and with 'remove_mask' from object_removal.py. Changed the initialization parameter of SeamImage.py to the location of an image instead of the numpy array representing the image.

--- SeamImage.py
+++ SeamImage.py
@@ -3,12 +3,16 @@ import cv2
 from skimage.io import imshow
 import matplotlib.pyplot as plt
 import time
-import numpy as np
 
 class SeamImage:
-    def __init__(self, image):
-        self.image = image  # The current state of the image, will change continuously
-        self.original_shape = image.shape
+    def __init__(self, location, color=True):
+        self.location = location
+        # self.image is the current state of the image, will change continuously
+        if color:
+            self.image = cv2.cvtColor(cv2.imread(location), cv2.COLOR_BGR2RGB)
+        else:
+            self.image = cv2.imread(location, cv2.IMREAD_GRAYSCALE)
+        self.original_shape = self.image.shape
 
         self.added_seams = []  # Stores np.arrays containing the row/col indices that were added
         self.added_order = []  # Will store True/False based on whether a row or col was added respectively
@@ -17,10 +21,17 @@ class SeamImage:
         self.removed_values = []  # Stores np.arrays containing the values that were removed
         self.removed_order = []  # Will store True/False based on whether a row or col was removed respectively
 
-    def remove_rows_and_cols(self, energy_function=sc.e1_colour_numba, rows=0, cols=0, mask=False):
+    def remove_rows_and_cols(self, energy_function=sc.e1_colour_numba, rows=0, cols=0):
+        """
+        Removes rows and/or columns to the image
+        :param energy_function: an energy function to apply for the seam carving
+        :param rows: amount of rows to remove
+        :param cols: amount of columns to remove
+        :return: Nothing
+        """
         while self.added_order and (rows > 0 or cols > 0):
             if self.added_order.pop():  # Removing an earlier added row
-                self.image = sc.remove_row_seam_numba(self.image, self.added_seams.pop(), mask )[0]
+                self.image = sc.remove_row_seam_numba(self.image, self.added_seams.pop())[0]
                 rows -= 1
             else:  # Removing an earlier added column
                 self.image = sc.remove_column_seam_numba(self.image, self.added_seams.pop())[0]
@@ -38,12 +49,19 @@ class SeamImage:
 
         # Remove the amount of rows and columns from the image that remain to be removed
         if rows > 0 or cols > 0:
-            self.image, new_values, new_seams, new_orders = sc.remove_rows_and_cols(self.image, energy_function, rows, cols, mask=mask)
+            self.image, new_values, new_seams, new_orders = sc.remove_rows_and_cols(self.image, energy_function, rows, cols)
             self.removed_values.extend(new_values)
             self.removed_seams.extend(new_seams)
             self.removed_order.extend(new_orders)
 
     def add_rows_and_cols(self, energy_function=sc.e1_colour, rows=0, cols=0):
+        """
+        Adds rows and/or columns to the image
+        :param energy_function: an energy function to apply for the seam carving
+        :param rows: amount of rows to add
+        :param cols: amount of columns to add
+        :return: Nothing
+        """
         while self.removed_order and (rows > 0 or cols > 0):
             if self.removed_order.pop():  # Adding an earlier removed row
                 self.image = sc.reconstruct_row_seam_numba(self.image, self.removed_values.pop(), self.removed_seams.pop())
@@ -72,26 +90,55 @@ class SeamImage:
             self.added_seams.extend(new_seams)
             self.added_order.extend([False for _ in range(cols)])
 
-    # Always add new rows or columns to the image, needed for the removal
-    def always_add_rows_or_columns(self, energy_function=sc.e1_colour, rows=0, cols=0):
-        # Add the amount of rows to the image that remain to be added
-        if rows > 0:
-            self.image, new_seams = sc.add_rows(self.image, energy_function, rows)
-            self.added_seams.extend(new_seams)
-            self.added_order.extend([True for _ in range(rows)])
+    def resize(self, energy_function, height, width):
+        """
+        Resizes the image to the specified format
+        :param energy_function: an energy function to apply for the seam carving
+        :param height: The height of the new image
+        :param width: The width of the new image
+        :return:
+        """
+        rows, cols = height-self.image.shape[0], width-self.image.shape[1]
+        # Add rows and columns
+        if rows >= 0 and cols >= 0:
+            self.add_rows_and_cols(energy_function=energy_function, rows=rows, cols=cols)
+        # Remove rows and columns
+        elif rows <= 0 and cols <= 0:
+            self.remove_rows_and_cols(energy_function=energy_function, rows=-rows, cols=-cols)
+        # Add rows, remove columns
+        elif rows >= 0 >= cols:
+            self.remove_rows_and_cols(energy_function=energy_function, rows=rows, cols=0)
+            self.add_rows_and_cols(energy_function=energy_function, rows=0, cols=-cols)
+        # Remove rows, add columns
+        else:
+            self.remove_rows_and_cols(energy_function=energy_function, rows=0, cols=cols)
+            self.add_rows_and_cols(energy_function=energy_function, rows=-rows, cols=0)
 
-        # Add the amount of columns to the image that remain to be added
-        if cols > 0:
-            self.image, new_seams = sc.add_columns(self.image, energy_function, cols)
-            self.added_seams.extend(new_seams)
-            self.added_order.extend([False for _ in range(cols)])
+    def remove_mask(self, mask, energy_function=sc.e1_colour_numba, keep_shape=False):
+        """
+        Removes a mask from an image
+        :param mask: a binary 2D array where the area to be removed is 1, the rest 0
+        :param energy_function: an energy function to apply for the seam carving
+        :param keep_shape: Boolean to indicate whether the original shape should be kept
+        :return: Nothing
+        """
+        original_rows, original_cols = self.image.shape[0], self.image.shape[1]
+        self.image = sc.remove_mask(self.image, energy_function=energy_function, mask=mask)
+        if keep_shape:
+            self.resize(energy_function, original_rows, original_cols)
+
 
 if __name__ == '__main__':
-    im = cv2.cvtColor(cv2.imread(".\\Figures\\Castle.jpg"), cv2.COLOR_BGR2RGB)
-    my_seam_image = SeamImage(im)
+    my_seam_image = SeamImage(location=".\\Figures\\Castle.jpg")
+    imshow(my_seam_image.image)
+    plt.show()
 
+    mask = cv2.imread('.\\Figures\\Castle_masked_person.jpg', cv2.IMREAD_GRAYSCALE)
+    _, mask = cv2.threshold(mask, 50, 1, cv2.THRESH_BINARY)
     start = time.time()
-    my_seam_image.add_rows_and_cols(rows=75, cols=150)
+    print(my_seam_image.image.shape)
+    my_seam_image.remove_mask(mask)
+    print(my_seam_image.image.shape)
     print(f'Time elapsed: {round(time.time() - start, 2)}')
     imshow(my_seam_image.image)
     plt.show()
@@ -107,5 +154,3 @@ if __name__ == '__main__':
     print(f'Time elapsed: {round(time.time() - start, 2)}')
     imshow(my_seam_image.image)
     plt.show()
-
-
