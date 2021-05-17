0139c5465a168419f618a22a1b9a223473580f5f
Timea Gyarmathy <gyarmathy.timi@hotmail.com>
2021-05-17 13:35:01 +0200

    Tkinter gui with initial drawing

--- gui/gui.py
+++ gui/gui.py
@@ -1,44 +1,67 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+
 import cv2
+import imutils
+import numpy as np
+from PIL import ImageTk, Image
 
-drawing = False  # true if mouse is pressed
-mode = True  # if True, draw rectangle. Press 'm' to toggle to curve
-ix, iy = -1, -1
+DEFAULT_IMAGE = '..\\Figures\\Castle.jpg'
 
 
-# mouse callback function
-def draw_circle(event, x, y, flags, param):
-    global ix, iy, drawing, mode
+class SeamCarvingGUI(tk.Frame):
+    def __init__(self, master):
+        super(SeamCarvingGUI, self).__init__(master)
+        self.pack()
+        self.master = master
 
-    if event == cv2.EVENT_LBUTTONDOWN:
-        drawing = True
-        ix, iy = x, y
+        self.frame = tk.Frame(self)
+        self.frame.pack(side=tk.TOP, expand=True, fill=tk.X)
 
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
-            if mode == True:
-                cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
-            else:
-                cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+        self.button = tk.Button(self.frame, text="OPEN", command=self.add_image)
+        self.button.pack(side=tk.LEFT)
 
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        if mode == True:
-            cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+        self.file_path = tk.StringVar()
+        self.label = tk.Label(self.frame, textvariable=self.file_path)
+        self.label.pack(side=tk.LEFT)
+
+        self.canvas = tk.Canvas(self, width=400, height=400, bg='black')
+        self.canvas.pack(fill=tk.BOTH)
+
+        self.image = ImageTk.PhotoImage(Image.fromarray(np.asarray([0, 0, 0])))
+        self.image_on_canvas = self.canvas.create_image(0, 0, image=self.image, anchor='nw')
+
+        self.canvas.bind("<Button-1>", self.get_x_and_y)
+        self.canvas.bind("<B1-Motion>", self.draw_smth)
+
+        self.lasx, self.lasy = None, None
+
+    def add_image(self):
+        img_path = filedialog.askopenfilename(initialdir=os.path.join(os.getcwd(), '..'), title="Open Image",
+                                              filetypes=[("Image Files", "*.png *.jpg *.jpeg")])
+        self.file_path.set(img_path)
+        cv_img = cv2.imread(img_path)
+        cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        height, width, _ = cv_img.shape
+        if width < height:
+            cv_img = imutils.resize(cv_img, width=400)
         else:
-            cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+            cv_img = imutils.resize(cv_img, height=400)
+        height, width, _ = cv_img.shape
+        self.image = ImageTk.PhotoImage(Image.fromarray(cv_img))
+        self.canvas.config(width=width, height=height)
+        self.canvas.itemconfig(self.image_on_canvas, image=self.image, anchor='nw')
+
+    def get_x_and_y(self, event):
+        self.lasx, self.lasy = event.x, event.y
+
+    def draw_smth(self, event):
+        self.canvas.create_line((self.lasx, self.lasy, event.x, event.y), fill='red', width=2)
+        self.lasx, self.lasy = event.x, event.y
 
 
-img = cv2.imread('dolphin.jpg')
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', draw_circle)
-
-while (1):
-    img = cv2.resize(img, (700, 500))
-    cv2.imshow('image', img)
-    k = cv2.waitKey(1) & 0xFF
-    if k == ord('m'):
-        mode = not mode
-    elif k == 27:
-        break
-
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    root = tk.Tk()
+    SeamCarvingGUI(master=root)
+    root.mainloop()
