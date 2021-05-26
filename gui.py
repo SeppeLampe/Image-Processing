dea626afbc65f166d2826f6c499fb801db21c732
Timea Gyarmathy <gyarmathy.timi@hotmail.com>
2021-05-26 11:40:37 +0200

    Added energy selection dropdown

--- gui.py
+++ gui.py
@@ -1,16 +1,21 @@
 import os
 import tkinter as tk
-from tkinter import ttk, messagebox
 from tkinter import filedialog
+from tkinter import ttk, messagebox
 
-import cv2
-import imutils
 import numpy as np
 from PIL import ImageTk, Image, ImageDraw
+
 from SeamImage import *
 
-DEFAULT_IMAGE = 'Figures\\Castle.jpg'
 DEFAULT_SIZE = 400
+
+ENERGY_FUNCTIONS = {
+    "e1": sc.e1_colour_numba,
+    "entropy": sc.entropy_colour_numba,
+    "hog": sc.hog_colour_numba
+}
+DROPDOWN_VALUES = ENERGY_FUNCTIONS.keys()
 
 
 class SeamCarvingGUI(tk.Frame):
@@ -49,6 +54,7 @@ class SeamCarvingGUI(tk.Frame):
         self.gradient_domain_amp_var = tk.IntVar()
         self.factor_var = tk.DoubleVar(value=1.2)
         self.output_img_name = tk.StringVar(value='result')
+        self.energy_function = tk.StringVar(value=list(ENERGY_FUNCTIONS.keys())[0])
 
         # Initialize GUI elements
         self.initialize_gui()
@@ -72,6 +78,20 @@ class SeamCarvingGUI(tk.Frame):
         top_frame = tk.Frame(self)
         top_frame.pack(side=tk.TOP, expand=True)
 
+        # Above everything, we have a dropdown for selecting the energy function
+        dropdown_frame = tk.Frame(top_frame)
+        dropdown_frame.pack(side=tk.TOP, expand=True, fill=tk.X)
+
+        label = tk.Label(dropdown_frame, text='Select energy function: ', anchor='w', font='Helvetica 12 bold')
+        label.pack(side=tk.LEFT)
+
+        energy_fc_dropdown = tk.OptionMenu(dropdown_frame, self.energy_function, *list(ENERGY_FUNCTIONS.keys()))
+        energy_fc_dropdown.pack(side=tk.LEFT)
+
+        # Separator lines are used between sections for better visibility
+        separator = ttk.Separator(top_frame, orient='horizontal')
+        separator.pack(fill=tk.X)
+
         # First section in the top frame contains components for selecting the image file
         file_frame = tk.Frame(top_frame)
         file_frame.pack(side=tk.TOP, expand=True, fill=tk.X)
@@ -82,7 +102,7 @@ class SeamCarvingGUI(tk.Frame):
         label = tk.Label(file_frame, anchor='w', textvariable=self.file_path)
         label.pack(side=tk.LEFT)
 
-        reset_button = tk.Button(file_frame, text="Reset", command=self.reset_img)
+        reset_button = tk.Button(file_frame, text="RESET", font='Helvetica 10 bold', command=self.reset_img)
         reset_button.pack(side=tk.RIGHT)
 
         # Separator lines are used between sections for better visibility
@@ -108,7 +128,8 @@ class SeamCarvingGUI(tk.Frame):
         height_entry = tk.Entry(resize_frame, textvariable=self.height_var)
         height_entry.pack(side=tk.LEFT)
 
-        gradient_domain_resize_checkbox = tk.Checkbutton(resize_frame, text="Gradient domain?", variable=self.gradient_domain_resize_var)
+        gradient_domain_resize_checkbox = tk.Checkbutton(resize_frame, text="Gradient domain?",
+                                                         variable=self.gradient_domain_resize_var)
         gradient_domain_resize_checkbox.pack(side=tk.LEFT)
 
         execute_resize_button = tk.Button(resize_frame, text="Execute", command=self.resize_image)
@@ -154,7 +175,8 @@ class SeamCarvingGUI(tk.Frame):
         factor_entry = tk.Entry(content_ampl_frame, textvariable=self.factor_var)
         factor_entry.pack(side=tk.LEFT)
 
-        gradient_domain_amplify_checkbox = tk.Checkbutton(content_ampl_frame, text="Gradient domain?", variable=self.gradient_domain_amp_var)
+        gradient_domain_amplify_checkbox = tk.Checkbutton(content_ampl_frame, text="Gradient domain?",
+                                                          variable=self.gradient_domain_amp_var)
         gradient_domain_amplify_checkbox.pack(side=tk.LEFT)
 
         execute_amplify_button = tk.Button(content_ampl_frame, text="Execute", command=self.amplify_content)
@@ -212,27 +234,26 @@ class SeamCarvingGUI(tk.Frame):
             return
 
         self.file_path.set(img_path)
-        
+
         if img_path.lower().endswith('.avi') or img_path.lower().endswith('.mpg') or img_path.lower().endswith('.mp4'):
             # Open video file, read first frame and initialize SeamImage
             self.cap = cv2.VideoCapture(img_path)
-            
+
             self.fps = self.cap.get(cv2.CAP_PROP_FPS)
 
-            
             # Read first frame
             ret, frame = self.cap.read()
-            
+
             if not ret:
                 print("Couldn't open video. Exiting now")
                 return
-            
+
             self.seam_image = SeamImage(None, color=True, image=frame)
-            
+
             # Create a temp output video
             self.temp_output_file_name = img_path[:-4] + '_temp.mp4'
             self.fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-            
+
         else:
             self.seam_image = SeamImage(img_path)
 
@@ -254,7 +275,7 @@ class SeamCarvingGUI(tk.Frame):
                 self.post_process()
             else:
                 # Go to first frame
-                self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0);
+                self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 # Read first frame
                 ret, frame = self.cap.read()
                 # Initialize SeamImage
@@ -352,32 +373,35 @@ class SeamCarvingGUI(tk.Frame):
             if self.cap is not None:
                 while True:
                     # Call the seam carving function on current frame
-                    self.seam_image.resize(height=target_height, width=target_width)
-                                      
+                    energy_fc = ENERGY_FUNCTIONS[self.energy_function.get()]
+                    self.seam_image.resize(height=target_height, width=target_width, energy_function=energy_fc)
+
                     # Store current frame output
                     if out is None:
                         out_video_size = (self.seam_image.image.shape[1], self.seam_image.image.shape[0])
                         out = cv2.VideoWriter(self.temp_output_file_name, self.fourcc, self.fps, out_video_size)
-                    
+
                     image = self.seam_image.get_image().astype(np.uint8)
                     # The returned image is in BGR
                     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                    
+
                     out.write(image)
-                    
+
                     # Read next frame
                     ret, frame = self.cap.read()
-                    
+
                     if not ret:
                         print("End of file. Exiting ...")
-                        break                   
-                    
-                    # Initialize seam_image to next frame
+                        break
+
+                        # Initialize seam_image to next frame
                     self.seam_image = SeamImage(None, color=True, image=frame)
             else:
                 # Call the seam carving function
-                self.seam_image.resize(height=target_height, width=target_width, gradient_domain=gradient_domain_resize_var)
-                
+                energy_fc = ENERGY_FUNCTIONS[self.energy_function.get()]
+                self.seam_image.resize(height=target_height, width=target_width,
+                                       gradient_domain=gradient_domain_resize_var, energy_function=energy_fc)
+
             # Update display
             self.post_process()
         except tk.TclError:
@@ -397,36 +421,41 @@ class SeamCarvingGUI(tk.Frame):
         green_mask = np.where(g, 1, 0)
 
         keep_shape = True if self.keep_shape_var.get() else False
-        
+
         if self.cap is not None:
             # Process all frames of a video
             out = None
             while True:
                 # Call the seam carving function on current frame
-                self.seam_image.remove_mask(mask_to_remove=red_mask, mask_to_keep=green_mask, keep_shape=keep_shape)
-                                  
+                energy_fc = ENERGY_FUNCTIONS[self.energy_function.get()]
+                self.seam_image.remove_mask(mask_to_remove=red_mask, mask_to_keep=green_mask, keep_shape=keep_shape,
+                                            energy_function=energy_fc)
+
                 # Store current frame output
                 if out is None:
                     out_video_size = (self.seam_image.image.shape[1], self.seam_image.image.shape[0])
                     out = cv2.VideoWriter(self.temp_output_file_name, self.fourcc, self.fps, out_video_size)
-                
+
                 image = self.seam_image.get_image().astype(np.uint8)
                 # The returned image is in BGR
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                
+
                 out.write(image)
-                
+
                 # Read next frame
                 ret, frame = self.cap.read()
-                
+
                 if not ret:
                     print("End of file. Exiting ...")
-                    break                   
-                
-                # Initialize seam_image to next frame
+                    break
+
+                    # Initialize seam_image to next frame
                 self.seam_image = SeamImage(None, color=True, image=frame)
         else:
-            self.seam_image.remove_mask(mask_to_remove=red_mask, mask_to_keep=green_mask, keep_shape=keep_shape)
+            # Call the seam carving function
+            energy_fc = ENERGY_FUNCTIONS[self.energy_function.get()]
+            self.seam_image.remove_mask(mask_to_remove=red_mask, mask_to_keep=green_mask, keep_shape=keep_shape,
+                                        energy_function=energy_fc)
 
         # Remove all drawings
         self.canvas.delete("line")
@@ -455,32 +484,36 @@ class SeamCarvingGUI(tk.Frame):
                 out = None
                 while True:
                     # Call the seam carving function on current frame
-                    self.seam_image.amplify_content(amp_factor=target_factor, gradient_domain=gradient_domain_amp_var)
-                                      
+                    energy_fc = ENERGY_FUNCTIONS[self.energy_function.get()]
+                    self.seam_image.amplify_content(amp_factor=target_factor, gradient_domain=gradient_domain_amp_var,
+                                                    energy_function=energy_fc)
+
                     # Store current frame output
                     if out is None:
                         out_video_size = (self.seam_image.image.shape[1], self.seam_image.image.shape[0])
                         out = cv2.VideoWriter(self.temp_output_file_name, self.fourcc, self.fps, out_video_size)
-                    
+
                     image = self.seam_image.get_image().astype(np.uint8)
                     # The returned image is in BGR
                     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                    
+
                     out.write(image)
-                    
+
                     # Read next frame
                     ret, frame = self.cap.read()
-                    
+
                     if not ret:
                         print("End of file. Exiting ...")
-                        break                   
-                    
-                    # Initialize seam_image to next frame
+                        break
+
+                        # Initialize seam_image to next frame
                     self.seam_image = SeamImage(None, color=True, image=frame)
             else:
                 # Call the seam carving function
-                self.seam_image.amplify_content(amp_factor=target_factor, gradient_domain=gradient_domain_amp_var)
-                
+                energy_fc = ENERGY_FUNCTIONS[self.energy_function.get()]
+                self.seam_image.amplify_content(amp_factor=target_factor, gradient_domain=gradient_domain_amp_var,
+                                                energy_function=energy_fc)
+
             # Update display
             self.post_process()
         except tk.TclError:
@@ -506,7 +539,6 @@ class SeamCarvingGUI(tk.Frame):
                 os.rename(self.temp_output_file_name, output_file_name)
             except Exception as e:
                 print(e)
-        
 
 
 if __name__ == '__main__':
